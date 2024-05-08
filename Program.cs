0b28ced743c4df3cc875c4a9ac4742f4badc4f51
Brennan <blfische92@gmail.com>
2024-05-08 22:08:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int readResult;
+            Console.WriteLine("Input number of elements to be contained in the array");
+            int myArrayLength = Convert.ToInt32(Console.ReadLine());
+            int[] myArray = new int[myArrayLength+1]; 
+            for( i = 0; i < myArrayLength; i++)
+            {
+
+                Console.WriteLine("Insert " + (i + 1) +  " element of the array");
+
+                myArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("the values stored are: ");
+            for (i = 0; i < myArrayLength; i++)
+            {
+
+                Console.WriteLine(myArray[i]);
+
+       
+            }
+            Console.WriteLine("the values stored in reverse are: ");
+            for (i = myArrayLength - 1; i >= 0; i--)
+            {
+
+                Console.WriteLine(myArray[i]);
+
+
+            }
+
+        }
+    }
+}
